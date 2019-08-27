c82035a733f4a959ade025d02701a6aa8ca34b78
Ruben Madila <contact@rubenmadila.com>
2019-08-27 19:41:05 +0100

    Initial commit

---
+++ typography.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Typography
+ * Plugin URI:      typography.rubenmadila.com
+ * Description:     A typography plugin for WordPress
+ * Author:          Ruben Madila
+ * Author URI:      rubenmadila.com
+ * Text Domain:     typography
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Typography
+ */
+
+// Your code starts here.
